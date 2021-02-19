ef9bd317f05e907baf279ae5d469e50a8bb73710
zzkung <zzkung@outlook.com>
2021-02-20 01:07:27 +0800

    ci

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React 1
         </a>
       </header>
     </div>
